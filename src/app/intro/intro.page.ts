4398035f71c3a06ce1b3d0f65047f46cad49dcce
Lucas Rodrigues Viana <lucasrviana@gmail.com>
2020-03-16 22:46:58 -0300

    project 5

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  constructor() { }
+  public slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+  ngOnInit() {
+  }
+
+}
