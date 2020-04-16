a087b6708f22c098da1a813e3a1b1ba638c790ba
Lucas Rodrigues Viana <lucasrviana@gmail.com>
2020-04-16 18:51:45 -0300

    cel

--- myApp/src/app/configuracoes/configuracoes.page.ts
+++ myApp/src/app/configuracoes/configuracoes.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-configuracoes',
   templateUrl: './configuracoes.page.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracoesPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
   }
 
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 }
